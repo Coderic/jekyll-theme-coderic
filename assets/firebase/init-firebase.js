d7fe62ef3dfa9916fbfa1d5141f03dbfceb2ed1b
Neftalí Yagua <despacho@neftaliyagua.com>
2020-11-01 23:56:38 -0500

    Agregar FireBase

---
+++ assets/firebase/init-firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA21ZM0KXw_Ys8iwzIzuMcUN1u8YfMsy9M",
+    authDomain: "coderic.firebaseapp.com",
+    databaseURL: "https://coderic.firebaseio.com",
+    projectId: "coderic",
+    storageBucket: "coderic.appspot.com",
+    messagingSenderId: "204370804567",
+    appId: "1:204370804567:web:46b70244eb62efe59a0812"
+};
+firebase.initializeApp(firebaseConfig);
